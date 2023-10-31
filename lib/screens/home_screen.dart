8353f4592473263fc9bcff649d984a644a8f647f
Sultan Amangeldiyev <sultanamangeldiyev@Sultans-MacBook-Pro.local>
2023-10-31 18:50:41 +0600

    notification, no IOS

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_messaging/services/notification_services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  NotificationServices notificationServices = NotificationServices();
+
+  @override
+  void initState() {
+    super.initState();
+    notificationServices.requestNotificationPermission();
+
+    notificationServices.getDeviceToken().then((value) {
+      print('device token\n' + value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
